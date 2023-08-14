ee9b5b89298ee241a446c42be133c5ab7a3ad526
Orlando Rodriguez <orlandog79@gmail.com>
2023-08-14 14:16:52 -0500

    update build folder

---
+++ build/routes/maintenanceRoute.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const maintenance_controllers_1 = require("../controllers/maintenance.controllers");
+const authjwt_1 = require("../middleware/authjwt");
+const router = (0, express_1.Router)();
+// Register new Maintenance
+router.post("/create-maintenance", authjwt_1.verifyToken, authjwt_1.isTech, maintenance_controllers_1.createMaintenance);
+//router.post("/create-maintenance", createMaintenance);
+exports.default = router;
