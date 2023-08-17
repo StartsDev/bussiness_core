4b9f64bc3544f708530799f0945efc19454e8d89
Orlando Rodriguez <orlandog79@gmail.com>
2023-08-17 18:30:05 -0500

    build

--- build/routes/imageRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const upload_controllers_1 = require("../controllers/upload.controllers");
-const router = (0, express_1.Router)();
-router.post('/upload-image/:id', upload_controllers_1.uploadControllerImg);
-exports.default = router;
