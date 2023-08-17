363be2a4af3fc0dc8bc743a1bad3247e32db16ca
Orlando Rodriguez <orlandog79@gmail.com>
2023-08-17 18:53:10 -0500

    build

--- build/interfaces/maintenance.interface.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.StatusOption = void 0;
-var StatusOption;
-(function (StatusOption) {
-    StatusOption["inProcess"] = "En proceso";
-    StatusOption["confirmed"] = "Confirmado";
-    StatusOption["autorized"] = "Autorizado";
-})(StatusOption || (exports.StatusOption = StatusOption = {}));
