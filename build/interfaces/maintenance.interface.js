838cb53b4675ad333db63f8478d65fc2e568dd13
Orlando Rodriguez <orlandog79@gmail.com>
2023-09-22 13:32:20 -0500

    propiety user_app clients service

--- build/interfaces/maintenance.interface.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.StatusOption = void 0;
-var StatusOption;
-(function (StatusOption) {
-    StatusOption["inProcess"] = "En proceso";
-    StatusOption["completed"] = "Completado";
-    StatusOption["confirmed"] = "Confirmado";
-})(StatusOption || (exports.StatusOption = StatusOption = {}));
